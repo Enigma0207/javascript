33a8f6045df4ced62069723a9995276ac480929c
alin <alinmansita@gmail.com>
2023-05-25 09:08:49 +0200

    introduction js

---
+++ exercice/js/exercice.js
@@ -0,0 +1,76 @@
+var prenom = "alin";
+// prenom est le nom de la variale , entre guillemet cest la valeur (ALIN) de la variabla, Type de la variable cest Stringla chaine de caracteur cad type de variable;
+// quand cest pas entre guillemet
+var nomMumero = 06616223;
+// type numérique pck ilya pas des guillemet
+var monMum = "0616223915";
+// entre guillemet cest une chaine de caractaire
+var booleanFals = false;
+// boollean
+var booleanTrue = true;
+// boolean
+// var varNull = null; // variable null
+// console.log(prenom);
+// syntaxe pour savoir le contenue de variable. avc ce syntaxe je vais savoir la valeur du variable, f12 et on clic sur console
+// alert("voici mon prenom alin");
+// prompt("Question: on est quel jour ?", "jour de la semaine");
+// avec le formulaire  Demander à l'utilisateur une valeur (2 façons)ici  on va aussi afficher une case formulaire ou on peut inserer des info
+
+//on met dedant le nom de la valeur et apres on ouvre html dans le navigateur,on va voir le texte voici  monprenom alin dans la boite de dialogue
+// window.alert("voici mon prenom alin");
+// ceci est aussi meme k alert(......)
+// document.write("A la pause vous aurez des Haribos Pik !");
+// on aura le text dans la page
+
+// j'ai declaré une variable mon prenom avec valeur prompt et pour voir la valeur ou resultat de ma variable on met le 2ieme syntaxte pour afficher la valeur dans la page
+// var monPrenom = prompt();
+// document.write(monPrenom);
+
+//
+// var unboolean = false;
+
+// console.log(unboolean);
+// ici dans le console cest true qui sera afficher car on a changé le boolean fals en true
+// unboolean = "true";
+// console.log(unboolean);
+// console.log(typeof unboolean);
+
+//les constant
+// const PAYS = "France";
+
+// document.write(PAYS);
+//    STRING NUMBER
+// var unChiffre = "12";
+// console.log(unChiffre);
+// console.log(typeof unChiffre);
+
+// CHANGER STRING EN NOMBRE AVEC SYNTAXE STRING EN NOMBRE
+// unChiffre = parseInt(unChiffre);
+// console.log(unChiffre);
+// console.log(typeof unChiffre);
+
+// pour decimal
+
+// je ré-affecte une valeur
+// unChiffre = "12.22";
+// console.log(unChiffre);
+// console.log(typeof unChiffre);
+// unChiffre = parseInt(unChiffre);
+
+// STRING => FLOAT ol converti en decimal
+// unChiffre = parseFloat(unChiffre);
+// console.log(unChiffre);
+// console.log(typeof unChiffre);
+
+// TYPE DE FOCTION
+
+/* a au moins un paramettre, sans valeur de retour, paramettre avec une valeur de retour
+mettre qlq part cad variable
+ sans paramettre, sans paramettre sans valeur de retour,*/
+
+var monPrenom;
+monPrenom = prompt();
+// page web 4
+document.write(monPrenom);
+console.log(monPrenom);
+alert(monPrenom);
