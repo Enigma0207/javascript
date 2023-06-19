0af189d543c78e25a92511c90170c01bec351548
alin <alinmansita@gmail.com>
2023-06-19 16:51:50 +0200

    js suite 19.06

---
+++ initiation/js/tour.js
@@ -0,0 +1,13 @@
+// Tournez manège
+// Ecrivez un programme manege.js qui fait faire 10 tours de manège en affichant un message à chaque tour.
+// C'est le tour de manège n°1
+// C'est le tour de manège n°2
+// C'est le tour de manège n°3
+// C'est le tour de manège n°4
+// C'est le tour de manège n°5
+// ...
+// Ensuite, améliorez votre programme pour que le nombre de tours soit saisi par l'utilisateur.
+// Testez avec des compteurs initialisés à d'autres valeurs que Zéro dans votre code.
+
+// Résultat :
+var tourManege = ["manège1", "manège2", "manège3", "manège4", "manège5"];
