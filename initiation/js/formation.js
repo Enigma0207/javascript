47e207fcbc81f3c13331e71e7a36c190b0af0e54
alin <alinmansita@gmail.com>
2023-06-27 16:33:22 +0200

    formation-js 27.06.23

---
+++ initiation/js/formation.js
@@ -0,0 +1,81 @@
+/*27 06 2023
+
+Écrire un programme informatique en Javascript qui calcule le volume d’un parallélépipède rectangle.On rappelle que la formule 
+pour calculer le volume est: Volume = largeur x longueur x hauteur.pour y arriver: 1.il faut déclarer 3 variables(largeur, longueur et hauteur) 
+et les initialiser avec des valeurs numériques 2.déclarer une variable volume 
+et l’initialiser à 03.Appliquer la formule puis affecter son résultat à la variable volume4.afficher le résultat sur la console*/
+
+// let volume = 0;
+// let largeur = "2";
+// let longuer = "3";
+// let hauteur = "1";
+// volume = largeur * longuer * hauteur;
+// console.log(volume);
+// document.write(volume);
+
+
+                                            
+// let age =prompt("saisir votre àge");
+// if (age < 5) {
+//     console.log("bb");
+// }
+// else if ((age >= 5) && (age <= 10)) {
+//     console.log("enfant");
+// }
+// else if ((age >= 11) && (age <= 18)) {
+//     console.log("ado");
+// }
+// else if (age > 18) {
+//     console.log("vieux")
+// }
+// else {
+//     console.log("inconnuto");
+// }
+
+// avec la condution switch
+
+
+// [16:21] iboudiokel (Invité)
+    
+// en utilsant la structure conditionnelle switch
+// ecrire un programme qui afficher le jour de la semaine en fontion d'un nombre donné
+// si nombre = 0 afficher dimanche.....
+
+
+// en ulisant la ternaire ecrire un programe qui affiche adulte si l'age est superieur ou egal a  18 et mineur si age est inferieur a 18
+
+//  jour = 5;
+// switch (jour) {
+//     case 0: 
+//         console.log("dimanche");
+//         break;
+//     case 1: 
+//         console.log("lundi");
+//         break;
+//     case 2:
+//         console.log("mardi");
+//         break
+//     case 3: 
+//         console.log("mercredi");
+//         break
+//     case 4:
+//         console.log("jeudi");
+        
+//         break;
+//     case 5: 
+//         console.log("vendredi");
+//         break;
+//     case 6:
+//         console.log("samedi")
+//         break
+    
+//     default:
+//         document.write("quel jour de la semaine")
+
+//         break;
+// }
+
+
+// var age = 5;
+// var message = (age >= 18) ? "majeur" : "mineur";
+// console.log(message);
