a64104c6c61d16920f7272df7e944417ac91c053
alin <alinmansita@gmail.com>
2023-07-03 16:58:30 +0200

    dom suite

---
+++ initiation/abraham/js/todo.js
@@ -0,0 +1,41 @@
+// // pour recuperer le bouton
+// let btn = document.getElementById("add_todo");
+// // pour recuperer le input
+// let todo = document.getElementById('todo');
+// // pour recuperer la div ou on va ajouter les todos
+// let todoList = document.getElementById("todolist");
+
+// // ecouter le click sur le bouton
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     // recuperer la valeur du input
+//     let val = todo.value;
+//     // creer un paragraphe
+//     let p = document.createElement("p");
+//     // ajouter le contenu du input dans le paragraphe
+//     p.textContent = val;
+//     // ajouter le paragraphe dans la div
+//     todoList.appendChild(p);
+//     //pour que le p soit clickable, il faut ca et pour changer la couler
+//     p.addEventListener("click", (e) => {
+//         // p.style.color = ('p.style.color!="red')? "red" :"bleu"
+//         p.style.color = "red";
+//         p.style.fontSize="2em"
+//     }
+
+//     )
+    
+// });
+
+
+// calculatrice
+
+let nombre1 = document.getElementById("number1");
+let button1 = document.getElementById("button1");
+let nombre2 = document.getElementById("number2");
+let button2 = document.getElementById("button2");
+button2.addEventListener("click", (e) => {
+    e.preventDefault();
+    let val=
+    
+})
--- initiation/js/objet.js
+++ initiation/js/objet.js
@@ -180,16 +180,16 @@
 // [13:56] iboudiokel (Invité)
     
 // ici on utilise une fonction traditionnelle
-// function rappel() {​​​​​​​
+// function rappel() {
 //     console.log("Ceci est une fonction de rappel")
-// }​​​​​​​
+// }
 
 
 // setTimeout(rappel(), 2000);
 
 
 // // ici on utilise une fonction anonyme
-// setTimeout(function() {​​​​​​​
+// setTimeout(function() {
 //     console.log("Ceci est une fonction de rappel")
 // }​​​​​​​, 2000)
 
@@ -198,4 +198,71 @@
 
 
 // LES DOM
+
+
+
+
+
+
+// BY STYLE
+
+// Sélectionnez l'élément par son ID
+const element1 = document.getElementById('p');
+
+// Définissez un tableau de couleurs
+const colors = ['red', 'green', 'blue', `yellow`, `black`, `orange`].reverse();
+
+// Définissez un index initial pour parcourir le tableau de couleurs
+let colorIndex = 0;
+
+// Définissez une fonction pour changer la couleur du texte
+function changeColor() {
+  // Récupérez la couleur correspondante à l'index actuel
+  const color = colors[colorIndex];
+
+  // Modifiez la couleur du texte de l'élément
+  element1.style.color = color;
+
+  // Incrémentez l'index pour passer à la couleur suivante
+  colorIndex++;
+
+  // Revenez au début du tableau si on atteint la fin
+  if (colorIndex === colors.length) {
+    colorIndex = 0;
+  }
+}
+
+// Appelez la fonction `changeColor` toutes les 1 seconde (1000 millisecondes)
+setInterval(changeColor, 1000);
+
+
+
+// Sélectionnez l'élément par son ID
+const element = document.getElementById('p');
+
+// Définissez un tableau de couleurs de fond
+const backgroundColors = ['red', 'green', 'blue', `yellow`, `black`, `orange`];
+
+// Définissez un index initial pour parcourir le tableau de couleurs de fond
+let backgroundColorIndex = 0;
+
+// Définissez une fonction pour changer la couleur de fond
+function changeBackgroundColor() {
+  // Récupérez la couleur de fond correspondante à l'index actuel
+  const backgroundColor = backgroundColors[backgroundColorIndex];
+
+  // Modifiez la couleur de fond de l'élément
+  element.style.backgroundColor = backgroundColor;
+
+  // Incrémentez l'index pour passer à la couleur de fond suivante
+  backgroundColorIndex++;
+
+  // Revenez au début du tableau si on atteint la fin
+  if (backgroundColorIndex === backgroundColors.length) {
+    backgroundColorIndex = 0;
+  }
+}
+
+// Appelez la fonction `changeBackgroundColor` toutes les 1 seconde (1000 millisecondes)
+setInterval(changeBackgroundColor, 1000);
 
